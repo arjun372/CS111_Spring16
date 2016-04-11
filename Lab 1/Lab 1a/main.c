4cf8853c9924c26e44ec48d9e67d85793e3efbfe
Arjun <arjun372@ucla.edu>
2016-04-10 22:55:34 -0700

    Lab1a 75% done

--- Lab 1/Lab 1a/main.c
+++ Lab 1/Lab 1a/main.c
@@ -12,6 +12,8 @@
 #define CATC_ERR 3
 #define FILE_MOD 0664
 
+#define _GNU_SOURCE /* for pipe2 */
+
 /* To get & set Terminal attributes */
 #include <termios.h>
 /* Argument Options parse headers */
@@ -34,29 +36,35 @@
 
 
 static struct termios TERM_INIT;
-static char BYTE;
 static char CR   = 0x0D;
 static char LF   = 0x0A;
 static char mEOF = 0x04;
+static char BYTE;
 
 static int VERBOSE = 0;
+static int SHELL   = 0;
 static char *ptr = NULL;
 
+static int I_PIPE_FD[2] = {-1, -1};
+static int O_PIPE_FD[2] = {-1, -1};
+
 static struct option long_options[] = {
-  {"shell", no_argument,          0, 's'},
-  {"verbose", no_argument, &VERBOSE,   1},
+  {"shell",   no_argument,   &SHELL, 1},
+  {"verbose", no_argument, &VERBOSE, 1},
 };
 
 /* function definitions */
-static void debug_log(const int opt_index, char **optarg, const int argc);
 static void setTC_initial();
 static void setTC_cooked();
+static void basic_mode();
+static void shell_mode();
 void sig_handler(int sigN);
 
 /* To be run at exit */
 int main(int argc, char **argv) {
 
- int ARGC = argc; char **ARGV = argv;
+ while(getopt_long_only(argc, argv, "", long_options, NULL) != -1)
+  continue;
 
  /* STEP 1/12 :: Save current terminal settings */
  /* STEP 2/12 :: atexit, set terminal to initial/cooked mode */
@@ -74,29 +82,100 @@ int main(int argc, char **argv) {
  if(tcsetattr(STDIN_FILENO, TCSANOW, &term_raw) != 0)
    fprintf(stderr, "FATAL :: Unable to switch to RAW mode, no guanrantees from this point on!\n");
 
+ if(SHELL)
+  shell_mode();
+ else
+  basic_mode();
 
+  exit(0);
+}
+
+static void shell_mode() {
+
+  /* Step 8/12 :: Catch SIGINT and forward it to shell */
+  if(signal(SIGINT, sig_handler) == SIG_ERR)
+      fprintf(stderr, "ERROR(%d): unable to catch SIGINT\n", errno);
+
+  if(signal(SIGINT, sig_handler) == SIG_ERR)
+    fprintf(stderr, "ERROR(%d): unable to catch SIGINT\n", errno);
+
+  char * cmds[] = {
+    "/bin/bash",
+    "-c",
+    "cat '/home/arjun/Desktop/file.html'",
+    NULL
+  };
+
+  int byte_written = 0; char bytes;
+  /* Create 2 pipes */
+  if(pipe2(I_PIPE_FD, O_CLOEXEC) < 0 || pipe2(O_PIPE_FD, O_CLOEXEC) < 0) /* Creating pipe failed */
+    exit(2);
+
+  pid_t pPID = getpid();
+  if(VERBOSE) fprintf(stderr, "Parent PID : %d\n", pPID);
+
+  /* Step 6/12 :: Fork a new process */
+  switch(pPID = fork())
+    {
+      case -1 : /* fork() failed */
+      fprintf(stderr, "FATAL: error forking child process\n");
+      exit(2);
+
+      case 0 : /* In child's process */
+      if(VERBOSE) fprintf(stderr, "Child PID : %d\n", getpid());
+      dup2(O_PIPE_FD[0], STDIN_FILENO);
+      dup2(I_PIPE_FD[1], STDOUT_FILENO);
+      dup2(I_PIPE_FD[1], STDERR_FILENO);
+      execvp(cmds[0], cmds);
+      break;
+
+      default:
+        while(read(STDIN_FILENO, &BYTE, 1))
+        {
+          /*Step 9/12 :: upon EOF, close pipe, send SIGHUP, restore & exit(0)*/
+          if(BYTE == mEOF)
+          {
+              close(O_PIPE_FD[0]);
+              close(O_PIPE_FD[1]);
+              kill(-2, SIGHUP);
+              exit(0);
+          }
+
+          /* Step 7/12 :: Pipe to shell, NO ECHO on PIPE */
+          if(BYTE == CR || BYTE == LF)
+          {
+            byte_written = write(STDOUT_FILENO, &CR, 1);
+            BYTE = LF;
+          }
+          byte_written = write(STDOUT_FILENO, &BYTE, 1);
+          byte_written = write(O_PIPE_FD[1], &BYTE, 1);
+        }
+        break;
+    }
+  return;
+}
+static void basic_mode() {
  /* Step 4/12 :: Read char-by-char  */
  /* Step 5/12 :: write char-by-char */
  int byte_written;
  while(read(STDIN_FILENO, &BYTE, 1))
  {
-   if(BYTE == 0x04)
+   if(BYTE == mEOF)
      break;
 
-   if(BYTE == CR || BYTE == LF) {
+   if(BYTE == CR || BYTE == LF)
+   {
      byte_written = write(STDOUT_FILENO, &CR, 1);
      BYTE = LF;
    }
-
    byte_written = write(STDOUT_FILENO, &BYTE, 1);
  }
-
-  exit(0);
 }
 
 void sig_handler(int sigN) {
-  fprintf(stderr, "Caught SIGNAL %d\n", sigN);
-  exit(CATC_ERR);
+  /* Step 8/12 [part 1/2] :: Send SIGINT to shell */
+  if(VERBOSE) fprintf(stderr,"CAUGHT SIGNAL %d, forwarding to child\n", sigN);
+  kill(-2, sigN);
 }
 
 static void setTC_initial() {
@@ -126,17 +205,4 @@ static void setTC_cooked() {
 
   close(STDIN_FILENO);
   close(STDOUT_FILENO);
-}
-
-/* Logs to stdout */
-static void debug_log(const int opt_index, char **optarg, const int argc)
-{
-  if(!VERBOSE)
-    return;
-
-  int i;
-  fprintf(stderr,"--%s", long_options[opt_index].name);
-  for(i = 0; i < argc; i++)
-    fprintf(stderr," %s", optarg[i]);
-  fprintf(stderr,"\n");
 }
