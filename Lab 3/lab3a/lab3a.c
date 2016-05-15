d236fc7aa68918db7b98be237916327a229f434b
Arjun Arjun <arjun372@ucla.edu>
2016-05-15 13:15:45 -0700

    its so beautiful

--- Lab 3/lab3a/lab3a.c
+++ Lab 3/lab3a/lab3a.c
@@ -80,7 +80,10 @@ static int fill_block(Block_t *toFill, int fd) {
         int bytesRead = 0;
         for(i = 0; i < (toFill->nDataObjects); i++) {
                 bytesRead += pread(fd, &(toFill->dataObjects[i].value), toFill->dataObjects[i].size, toFill->dataObjects[i].offset);
-                if (VERBOSE) fprintf(stderr, toFill->dataObjects[i].format, toFill->dataObjects[i].value);
+                if (VERBOSE) {
+                        fprintf(stderr, toFill->dataObjects[i].format, toFill->dataObjects[i].value);
+                        fprintf(stderr, ",");
+                }
         }
         if (VERBOSE) fprintf(stderr, "TOTAL READS (IDEAL:34) = %d\n", bytesRead);
         return bytesRead;
