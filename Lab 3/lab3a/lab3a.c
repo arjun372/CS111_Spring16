1285b25383f3ea912fe1a99a8ffcbe4ccbe8c2da
Arjun Arjun <arjun372@ucla.edu>
2016-05-16 12:59:17 -0700

    lets see man

--- Lab 3/lab3a/lab3a.c
+++ Lab 3/lab3a/lab3a.c
@@ -239,7 +239,7 @@ static int fill_GroupDescriptors(const int fd, GroupDescriptor_t **gdTable, cons
                 /* read from disk */
                 //fill_block(fd, gdTable[i]->dataObjects, gdTable[i]->nDataObjects);
                 for(j = 0; j < (gdTable[i]->nDataObjects); j++) {
-                        pread(fd, &(gdTable[i]->dataObjects[j].value), gdTable[i]->dataObjects[j].size, gdTable[i]->dataObjects[j].offset);
+                        //        pread(fd, &(gdTable[i]->dataObjects[j].value), gdTable[i]->dataObjects[j].size, gdTable[i]->dataObjects[j].offset);
                         fprintf(stderr, gdTable[i]->dataObjects[j].format, gdTable[i]->dataObjects[j].value);
                         fprintf(stderr, (j == (gdTable[i]->nDataObjects - 1)) ? "\n" : ",");
                 }
