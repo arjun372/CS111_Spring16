ff6c1d436611083a9913004b3a4c6711bff223d1
Arjun Arjun <arjun372@ucla.edu>
2016-05-19 15:50:12 -0700

    rec_en

--- Lab 3/lab3a/lab3a.c
+++ Lab 3/lab3a/lab3a.c
@@ -337,7 +337,7 @@ static uint32_t dir_doWrite2(int readfd, int writefd,  uint32_t parentInode, uin
                                 *inode_num,               // inode number of file
                                 name);                   // name
 
-                if(*name_len + 8 <= *rec_len)
+                if(*rec_len - (*name_len + 8) <= 0)
                         break;
         }
         free(inode_num);
