7a9e2920e195bc7f02bf09be0b2805087310354d
Arjun Arjun <arjun372@ucla.edu>
2016-05-18 22:57:50 -0700

    include <inttypes.h>

--- Lab 3/lab3a/lab3a.c
+++ Lab 3/lab3a/lab3a.c
@@ -15,7 +15,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>  /* for fprintf used in debug_log */
-
+#include <inttypes.h>
 #include "lab3a.h"
 
 static SuperBlock_t      *SUPERBLOCK_TABLE;
