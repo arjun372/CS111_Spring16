cb05fe35c6531a4c6bf16f6bfa80a824a474c84e
Rahul Malavalli <rahul.malavalli@gmail.com>
2016-05-25 18:47:16 -0700

    initStructs

--- Lab 3/lab3b/lab3b.py
+++ Lab 3/lab3b/lab3b.py
@@ -66,6 +66,10 @@ INCORRECT_LINKCOUNT_INODES   = []
 INCORRECT_DIRECTORY_ENTRIES  = []
 DUPLICATELY_ALLOCATED_BLOCKS = []
 
+
+# Super block entries
+MagicNumber,InodeCount,BlockCount,BlockSize,FragmentSize,BlocksPerGroup,InodesPerGroup,FragmentsPerGroup,FirstDataBlock = tuple([None]*9)
+
 # Read CSV files into program
 superblock       = csv.reader(open('super.csv', 'rb'), delimiter=',', quotechar='"');
 group_descriptor = csv.reader(open('group.csv', 'rb'), delimiter=',', quotechar='"');
@@ -74,80 +78,68 @@ inode            = csv.reader(open('inode.csv', 'rb'), delimiter=',');
 directory        = csv.reader(open('directory.csv', 'rb'), delimiter=',');
 indirect_blocks  = csv.reader(open('indirect.csv', 'rb'), delimiter=',');
 
-# parse superblock data
-for line in superblock:
-    MagicNumber       = int(line[0], 16);
-    InodeCount        = int(line[1]);
-    BlockCount        = int(line[2]);
-    BlockSize         = int(line[3]);
-    FragmentSize      = int(line[4]);
-    BlocksPerGroup    = int(line[5]);
-    InodesPerGroup    = int(line[6]);
-    FragmentsPerGroup = int(line[7]);
-    FirstDataBlock    = int(line[8]);
+def initStructs():
+    # parse superblock data
+    for line in superblock:
+        MagicNumber       = int(line[0], 16);
+        InodeCount        = int(line[1]);
+        BlockCount        = int(line[2]);
+        BlockSize         = int(line[3]);
+        FragmentSize      = int(line[4]);
+        BlocksPerGroup    = int(line[5]);
+        InodesPerGroup    = int(line[6]);
+        FragmentsPerGroup = int(line[7]);
+        FirstDataBlock    = int(line[8]);
 
-# parse group descriptor data : information for each group descriptor
-for line in group_descriptor:
-    free_inode_bitmap_block = int(line[4], 16)
-    free_block_bitmap_block = int(line[5], 16)
-    BitmapPointers_FreeInodes.append(free_inode_bitmap_block)
-    BitmapPointers_FreeBlocks.append(free_block_bitmap_block)
+    # parse group descriptor data : information for each group descriptor
+    for line in group_descriptor:
+        free_inode_bitmap_block = int(line[4], 16)
+        free_block_bitmap_block = int(line[5], 16)
+        BitmapPointers_FreeInodes.append(free_inode_bitmap_block)
+        BitmapPointers_FreeBlocks.append(free_block_bitmap_block)
 
-# parse free_bitmap_entry data : list of free inodes and free blocks
-for line in bitmap:
-    MapBlock_Number       = int(line[0], 16);
-    Block_or_Inode_Number = int(line[1]);
-    if   MapBlock_Number in BitmapPointers_FreeInodes: FreeInodes.append(Block_or_Inode_Number)
-    elif MapBlock_Number in BitmapPointers_FreeBlocks: FreeBlocks.append(Block_or_Inode_Number)
-    elif (verbose == True): print "MapBlock_Number: %s is not present in either bitmap file!!" % MapBlock_Number
+    # parse free_bitmap_entry data : list of free inodes and free blocks
+    for line in bitmap:
+        MapBlock_Number       = int(line[0], 16);
+        Block_or_Inode_Number = int(line[1]);
+        if   MapBlock_Number in BitmapPointers_FreeInodes: FreeInodes.append(Block_or_Inode_Number)
+        elif MapBlock_Number in BitmapPointers_FreeBlocks: FreeBlocks.append(Block_or_Inode_Number)
+        elif (verbose == True): print "MapBlock_Number: %s is not present in either bitmap file!!" % MapBlock_Number
 
-# parse indirect block entry: These are all the non-zero block pointers in an indirect block.
-#                             The blocks that contain indirect block pointers are included.
-for line in indirect_blocks:
-    ContainingBlockNumber             = int(line[0], 16)
-    (EntryNumber, BlockPointer_Value) = (int(line[1]), int(line[2], 16))
-    if ContainingBlockNumber not in INDIRECT_BLOCKS:
-        INDIRECT_BLOCKS[ContainingBlockNumber] = [(EntryNumber, BlockPointer_Value)]
-    else:
-        INDIRECT_BLOCKS[ContainingBlockNumber].append((EntryNumber, BlockPointer_Value))
+    # parse indirect block entry: These are all the non-zero block pointers in an indirect block.
+    #                             The blocks that contain indirect block pointers are included.
+    for line in indirect_blocks:
+        ContainingBlockNumber             = int(line[0], 16)
+        (EntryNumber, BlockPointer_Value) = (int(line[1]), int(line[2], 16))
+        if ContainingBlockNumber not in INDIRECT_BLOCKS:
+            INDIRECT_BLOCKS[ContainingBlockNumber] = [(EntryNumber, BlockPointer_Value)]
+        else:
+            INDIRECT_BLOCKS[ContainingBlockNumber].append((EntryNumber, BlockPointer_Value))
 
-if MagicNumber != 0xef53 : print 'This doesnt appear to be an EXT2 Filesytem. No guarantees from this point on...'
-
-
-
-
-# UNALLOCATED BLOCK : Blocks that are in use but also listed on the free bitmap.
-#                     Here the INODEs should be listed in increasing order of inode_num.
-
-
-# UNALLOCATED_DIRECTORY :
+    if MagicNumber != 0xef53 : print 'This doesnt appear to be an EXT2 Filesytem. No guarantees from this point on...'
 
 
 
 
+    # UNALLOCATED BLOCK : Blocks that are in use but also listed on the free bitmap.
+    #                     Here the INODEs should be listed in increasing order of inode_num.
 
-# Missing iNode :: inodes that are not in use, and not listed on the free bitmap
+
+    # UNALLOCATED_DIRECTORY :
 
 
 
 
 
-# Open file to write the resulting output
-output_file = open('lab3b_check.txt', 'w+')
-
-for item in UNALLOCATED_BLOCKS : output_file.write("UNALLOCATED BLOCK < " + 1035 + " > REFERENCED BY INODE < " + 16 + " > ENTRY < " + 0 + " > INODE < " + 17 + " > INDIRECT BLOCK < " + 10 + " > ENTRY < " + 0 + " >\n")
+    # Missing iNode :: inodes that are not in use, and not listed on the free bitmap
 
 
-# if verbose == True:
-#     for line in superblock: print line
-#     for line in group_descriptor: print line
-#     for line in bitmap: print line
-#     for line in inode: print line
-#     for line in directory: print line
-#     for line in indirect_blocks: print line
-#     # for line in superblock: fout.write(str(line))
-#     # for line in group_descriptor: fout.write(str(line))
-#     # for line in bitmap: fout.write(str(line))
-#     # for line in inode: fout.write(str(line))
-#     # for line in directory: fout.write(str(line))
-#     # for line in indirect_blocks: fout.write(str(line))
+if __name__ == "__main__":
+    # Open file to write the resulting output
+    output_file = open('lab3b_check.txt', 'w+')
+
+    initStructs()
+
+    for item in UNALLOCATED_BLOCKS : output_file.write("UNALLOCATED BLOCK < " + 1035 + " > REFERENCED BY INODE < " + 16 + " > ENTRY < " + 0 + " > INODE < " + 17 + " > INDIRECT BLOCK < " + 10 + " > ENTRY < " + 0 + " >\n")
+
+    
